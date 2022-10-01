04100643fc513c3225a7fcc05f6ca014bdc9cc18
Pavel A. Kulagin <114352554+pqual@users.noreply.github.com>
2022-10-01 13:40:26 +0300

    Lab 2

---
+++ stock/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class BuySellForm(forms.Form):
+    price = forms.DecimalField(widget=forms.NumberInput(
+        attrs={'readonly':'readonly'}
+    ))
+    amount = forms.IntegerField()
